a2ac24495a5cf898ee98d507a28d04a3c4e4991d
Charles Loureiro de Deus <charliedeus@gmail.com>
2019-10-28 15:21:16 -0300

    Create a root component

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-const soma = (a, b) => a + b;
+import React from 'react';
+import { render } from 'react-dom';
 
-alert(soma(1, 4));
+import App from './app';
+
+render(App, document.getElementById('app'));
